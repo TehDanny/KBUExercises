177365861e037a9ff5523643eb3920c9b699cd9d
Daniel Alexander Nedergaard <daniel.nedergaard@uptime.dk>
2018-04-05 13:20:31 +0200

    Completed question 1 and 2

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -16,8 +16,11 @@ namespace Exercises
 
         private void Run()
         {
-            int middle = Middle(3, 4, 5);
+            int middle = Middle4(3, 4, 5);
             Console.WriteLine(middle);
+            middle = Middle4(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            Console.WriteLine(middle);
+            Console.WriteLine(ValueExists());
             Console.ReadKey();
         }
 
@@ -30,6 +33,84 @@ namespace Exercises
             if (z > x && z < y || z < x && z > y)
                 return z;
             return 0;
+        }
+
+        public int Middle2(int x, int y, int z)
+        {
+            int[] numArr = { x, y, z };
+            for (int i = 0; i < numArr.Length - 1; i++)
+            {
+                if (numArr[i] != numArr.Min() && numArr[i] != numArr.Max())
+                    return numArr[i];
+            }
+            return 0;
+        }
+
+        /* // recursiv
+        public int Middle3(int x, int y, int z)
+        {
+            int middle = 0;
+            if (middle)
+                return middle;
+            else
+                return Middle3();
+        }
+        */
+
+        public int Middle4(int x, int y, int z)
+        {
+            int[] numArr = { x, y, z };
+            return CalcMiddle(numArr);
+        }
+
+        public int Middle4(int a, int b, int c, int d, int e, int f, int g, int h, int i)
+        {
+            int[] numArr = { a, b, c, d, e, f, g, h, i };
+            return CalcMiddle(numArr);
+        }
+
+        private int CalcMiddle(int[] numArr)
+        {
+            int middleIndex = (numArr.Length - 1) / 2;
+            BubbleSort(numArr);
+            int middleNumber = numArr[middleIndex];
+
+            return middleNumber;
+        }
+
+        private int[] BubbleSort(int[] numArr)
+        {
+            bool hasSwapped;
+            int tempValue;
+
+            do
+            {
+                hasSwapped = false;
+
+                for (int i = 0; i < numArr.Length - 1; i++)
+                {
+                    if (numArr[i] > numArr[i + 1])
+                    {
+                        tempValue = numArr[i];
+                        numArr[i] = numArr[i + 1];
+                        numArr[i + 1] = tempValue;
+                        hasSwapped = true;
+                    }
+                }
+            } while (hasSwapped == true);
+            return numArr;
+        }
+
+        public bool ValueExists()
+        {
+            double x = 2;
+            int[] t = { 1, 2, 3 };
+            for (int i = 0; i < t.Length -1; i++)
+            {
+                if (x.Equals(t[i]))
+                    return true;
+            }
+            return false;
         }
     }
 }
