38d882c7d7c359bf55f98898bf41cb89d1520d42
Daniel Alexander Nedergaard <daniel.nedergaard@uptime.dk>
2018-04-05 11:59:04 +0200

    Added an awful Middle function

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -10,6 +10,26 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+            int middle = Middle(3, 4, 5);
+            Console.WriteLine(middle);
+            Console.ReadKey();
+        }
+
+        public int Middle(int x, int y, int z)
+        {
+            if (x > y && x < z || x < y && x > z)
+                return x;
+            if (y > x && y < z || y < x && y > z)
+                return y;
+            if (z > x && z < y || z < x && z > y)
+                return z;
+            return 0;
         }
     }
 }
